b88e03498fb05473a50ff8840c094eba7ad23e38
Semirekov Evgenii <ekar@yandex.ru>
2022-08-30 11:13:17 +0300

    Add specs to PokemonEntity model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,11 @@ class Pokemon(models.Model):
     '''Покемон'''
     title = models.CharField('Покемон', max_length=200)
     photo = models.ImageField(upload_to='pokemons', null=True)
+    level = models.IntegerField('Уровень', null=True)
+    health = models.IntegerField('Здоровье', null=True)
+    strenght = models.IntegerField('Атака', null=True)
+    defence = models.IntegerField('Защита', null=True)
+    stamina = models.IntegerField('Выносливость', null=True)
 
     def __str__(self):
         return f'{self.title}'
