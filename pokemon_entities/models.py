74d81b7c4f94a9e270b22421d44e69e1ef7e3e09
Semirekov Evgenii <ekar@yandex.ru>
2022-08-31 16:46:03 +0300

    Исправил ошибку в схеме БД. Перенес поля в таблицу сущностей.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,23 +5,23 @@ class Pokemon(models.Model):
     '''Покемон'''
     title = models.CharField('Покемон', max_length=200)
     photo = models.ImageField(upload_to='pokemons', null=True)
-    level = models.IntegerField('Уровень', null=True)
-    health = models.IntegerField('Здоровье', null=True)
-    strenght = models.IntegerField('Атака', null=True)
-    defence = models.IntegerField('Защита', null=True)
-    stamina = models.IntegerField('Выносливость', null=True)
-
+    
     def __str__(self):
         return f'{self.title}'
 
 class PokemonEntity(models.Model):
-    '''Положение на карте'''
-    lat = models.FloatField('Широта', null=True)
-    lon = models.FloatField('Долгота', null=True)
+    '''Положение на карте'''    
     pokemon = models.ForeignKey(
         Pokemon,
         null=False,
         verbose_name='Покемон',
         on_delete=models.CASCADE)
+    lat = models.FloatField('Широта', null=True)
+    lon = models.FloatField('Долгота', null=True)
+    level = models.IntegerField('Уровень', null=True)
+    health = models.IntegerField('Здоровье', null=True)
+    strenght = models.IntegerField('Атака', null=True)
+    defence = models.IntegerField('Защита', null=True)
+    stamina = models.IntegerField('Выносливость', null=True)
     appeared_at = models.DateTimeField(null=False)
     disappeared_at = models.DateTimeField(null=True)
